e376bdfd56e7256d6a2b270346ed4196cac2a4cd
HassanSep21 <slayerlol000@gmail.com>
2023-08-12 11:59:46 +0500

    Create encryption-decyption.py

---
+++ encryption-decyption.py
@@ -0,0 +1,31 @@
+
+import random
+import string
+
+char = " " + string.punctuation + string.digits + string.ascii_letters
+char = list(char)
+key = char.copy()
+random.shuffle(key)
+
+# print(f"char: {char}")
+# print(f"key : {key}")
+
+# encyption
+plain_txt = input("Enter yout text: ")
+cypher_txt = ""
+
+for letter in plain_txt:
+    index = char.index(letter)
+    cypher_txt += key[index]
+
+print(f"Encrypted text: {cypher_txt}")
+
+# decryption
+cypher_txt = input("Enter yout cypher text: ")
+plain_txt = ""
+
+for letter in cypher_txt:
+    index = key.index(letter)
+    plain_txt += char[index]
+
+print(f"Decrypted text: {plain_txt}")
